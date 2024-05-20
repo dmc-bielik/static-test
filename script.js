9bde92564a2e03e804d1e4612feec6ff39e0ae4a
David Bielik <davidbielik@deomwano.com>
2024-05-20 12:31:00 -0400

    test for javascript

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const div1 = document.getElementById("div1")
-const div2 = document.getElementById("div2")
+/*
 const button1 = document.getElementById("button1")
 const button2 = document.getElementById("button2")
+const button3 = document.getElementById("button3")
+const button4 = document.getElementById("button4")
+const button5 = document.getElementById("button5")
 
-function changeDisplay(div, list) {
-  for (const element of list) {
-    if (div === element) {
-      element.style.display = "flex"
-    } else {
-      element.style.display = "none"
-    }
-  }
-}
 
-const divList = [div1, div2]
 
-button1.addEventListener("click", function() {
-  changeDisplay(div1, divList)
+
+
+button3.addEventListener("click", function() {
+    document.getElementById("div3").scrollIntoView({behavior: 'smooth'});
 })
 
-button2.addEventListener("click", function() {
-    changeDisplay(div2, divList)
-  })
+button4.addEventListener("click", function() {
+    document.getElementById("div4").scrollIntoView({behavior: 'smooth'});
+})
+
+button5.addEventListener("click", function() {
+    document.getElementById("div5").scrollIntoView({behavior: 'smooth'});
+})
+
+*/
